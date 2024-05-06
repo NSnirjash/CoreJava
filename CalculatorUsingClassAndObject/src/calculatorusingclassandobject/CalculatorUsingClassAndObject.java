bf6d9e537267bf501fb149596977aad4fa7e347c
NSnirjash <158472502+NSnirjash@users.noreply.github.com>
2024-05-06 17:25:50 +0600

    Java (06-05-2024)
    Practice Java

---
+++ CalculatorUsingClassAndObject/src/calculatorusingclassandobject/CalculatorUsingClassAndObject.java
@@ -0,0 +1,16 @@
+
+package calculatorusingclassandobject;
+
+import calculator.Calculation;
+
+
+public class CalculatorUsingClassAndObject {
+
+   
+    public static void main(String[] args) {
+        Calculation c= new Calculation(30, 10, 2);
+                System.out.println(c.useCalculator());
+                        
+    }
+    
+}
