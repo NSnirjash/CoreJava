6360ac5f245c5aee0d8a584e7c637254f81808f5
NSnirjash <158472502+NSnirjash@users.noreply.github.com>
2024-05-21 04:18:44 +0600

    Java(09-05-2024)

--- TestArray/src/testarray/TestArrayWithMethod.java
+++ TestArray/src/testarray/TestArrayWithMethod.java
@@ -9,16 +9,18 @@ public class TestArrayWithMethod {
         int[] myList = new int[20];
 
         for (int i = 0; i < myList.length; i++) {
+           
             Random r = new Random();
             int num = r.nextInt(100);
             myList[i] = num;
+        
         }
         System.out.println("The Array is: "+Arrays.toString(myList));
         
         int maxNum = maxNumber(myList);
         System.out.println("max: "+maxNum);
-        minNumber(myList);
         
+        minNumber(myList);
         
         System.out.println("Average: "+Average(myList));
 
@@ -26,30 +28,43 @@ public class TestArrayWithMethod {
 
     public static int maxNumber(int[] myList) {
         int maxNum = myList[0];
+       
         for (int i = 0; i < myList.length; i++) {
+            
             if (maxNum < myList[i]) {
+               
                 maxNum = myList[i];
             }
         }
         return maxNum;
     }
+    
+    
     public static void minNumber(int[] myList){
         int MinNum = myList[0];
+        
         for(int i = 0; i<myList.length; i++){
+            
             if(MinNum>myList[i]){
+                
                 MinNum=myList[i];
             }
         }
         System.out.println("min: "+MinNum);
     }
+    
+    
     public static int Average(int[] myList){
         int sum = 0;
         int avg = 0;
         
         for(int value : myList){
+            
             sum += value;
         }
+        
         avg = sum/myList.length;
+       
         return avg;
     }
 }
