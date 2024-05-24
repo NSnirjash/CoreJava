d70407556874d350103ddbcd9935846cdb843a36
NSnirjash <158472502+NSnirjash@users.noreply.github.com>
2024-05-24 06:33:16 +0600

    Practice ExHandling

--- Exception Handling/ExceptionOddNumber/src/exceptionoddnumber/ExceptionOddNumberUsingMethod.java
+++ Exception Handling/ExceptionOddNumber/src/exceptionoddnumber/ExceptionOddNumberUsingMethod.java
@@ -8,25 +8,26 @@ package exceptionoddnumber;
 import java.util.Scanner;
 
 public class ExceptionOddNumberUsingMethod {
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter a number");
         int n = s.nextInt();
         CheckNumber(n);
-        
+
     }
-    
-    public static void CheckNumber(int n){
-        try{
+
+    public static void CheckNumber(int n) {
+        try {
             checkEvenNumber(n);
-            System.out.println(n+" is even number");
-        }catch(Exception e){
-            System.out.println("Error; "+e.getMessage());
+            System.out.println(n + " is even number");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
-    
+
     public static void checkEvenNumber(int n) throws Exception {
-        if(n%2!=0){
+        if (n % 2 != 0) {
             throw new Exception("Odd number is not granted");
         }
     }
