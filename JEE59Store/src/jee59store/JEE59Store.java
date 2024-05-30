fe8262369c823b7dd97d9954af8eadcdb0ddfa91
NSnirjash <158472502+NSnirjash@users.noreply.github.com>
2024-05-30 07:52:43 +0600

    JavaSwing

---
+++ JEE59Store/src/jee59store/JEE59Store.java
@@ -0,0 +1,10 @@
+
+package jee59store;
+
+public class JEE59Store {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ TestJDBC59/src/testjdbc59/TestJDBC59.java
@@ -0,0 +1,10 @@
+
+package testjdbc59;
+
+public class TestJDBC59 {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
